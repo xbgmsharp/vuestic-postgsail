e7206d671962284ef43fd9194a5930f476498748
Leon <leonronssin@appnovation.com>
2022-08-27 15:54:09 -0500

    navbar, filtering table, leaflet map

--- src/components/maps/leafletMap.vue
+++ src/components/maps/leafletMap.vue
@@ -1,44 +1,14 @@
 <template>
-  <div>
-    <l-map ref="leafmap" :zoom="mapZoom" :center="mapCenter" :bounds="mapBounds" style="height: 100%; width: 100%">
-      <l-tile-layer :url="url" :attribution="attribution" />
-      <template v-for="(pos, index) in mapMarkers" :key="index">
-        <l-marker :lat-lng="pos" />
-      </template>
-      <l-polyline :lat-lngs="mapPath" color="red"></l-polyline>
-    </l-map>
-  </div>
+  <div id="mapContainer"></div>
 </template>
 
 <script>
-  import { latLng, latLngBounds } from 'leaflet'
-  import { LMap, LTileLayer, LMarker, LPolyline } from '@vue-leaflet/vue-leaflet'
   import 'leaflet/dist/leaflet.css'
-  import { defineComponent } from 'vue'
-  export default defineComponent({
-    components: {
-      LMap,
-      LTileLayer,
-      LMarker,
-      LPolyline,
-    },
+  import L from 'leaflet'
+
+  export default {
+    name: 'LeafletMap',
     props: {
-      loading: {
-        type: Boolean,
-        default: false,
-      },
-      center: {
-        type: Object,
-        default: null,
-      },
-      zoom: {
-        type: Number,
-        default: 6,
-      },
-      options: {
-        type: Object,
-        default: () => ({}),
-      },
       markers: {
         type: Array,
         default: () => [],
@@ -50,42 +20,40 @@
     },
     data() {
       return {
-        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        defaultOptions: {
-          zoomSnap: 0.5,
-        },
+        map: null,
       }
     },
-    computed: {
-      mapCenter() {
-        const [lat, lng] = this.center ? this.center : this.markers.length ? this.markers[0] : [0, 0]
-        return this.mapBounds ? this.mapBounds.getCenter() : lat && lng ? latLng(lat, lng) : latLng(0, 0)
-      },
-      mapZoom() {
-        return !this.mapBounds ? this.zoom : undefined
-      },
-      mapMarkers() {
-        return this.markers.map(([lat, lng]) => (lat && lng ? latLng(lat, lng) : null)).filter(Boolean)
-      },
-      mapPath() {
-        return this.path.map(([lat, lng]) => (lat && lng ? latLng(lat, lng) : null)).filter(Boolean)
-      },
-      mapBounds() {
-        const bounds = latLngBounds()
-        this.mapMarkers.forEach((position) => {
-          bounds.extend(position)
-        })
-        return this.mapMarkers.length && bounds.isValid() ? bounds : null
-      },
-      mapOptions() {
-        return {
-          ...this.defaultOptions,
-          ...this.options,
-        }
-      },
+    mounted() {
+      this.map = L.map('mapContainer').setView([46.05, 11.05], 5)
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map)
+      //use a mix of renderers
+      var customPane = this.map.createPane('customPane')
+      var canvasRenderer = L.canvas({ pane: 'customPane' })
+      customPane.style.zIndex = 399 // put just behind the standard overlay pane which is at 400
+      const markers = this.markers.map(([lat, lng]) => L.latLng(lat, lng))
+      markers.forEach((m) => {
+        L.marker(m).addTo(this.map)
+      })
+      if (this.path.length) {
+        const path = this.path.map(([lat, lng]) => L.latLng(lat, lng))
+        L.polyline(path, { color: 'red' }).addTo(this.map)
+      }
+      const bounds = L.latLngBounds(markers)
+      this.map.fitBounds(bounds, { padding: [40, 40] })
     },
-  })
+    onBeforeUnmount() {
+      if (this.map) {
+        this.map.remove()
+      }
+    },
+  }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  #mapContainer {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>

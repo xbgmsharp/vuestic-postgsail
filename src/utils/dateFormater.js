f705da81f04c5d1309dec7dc2baf7853d3907449
Leon <leonronssin@appnovation.com>
2022-09-07 10:56:18 -0500

    remove mixins and read dates as UTC

--- src/utils/dateFormater.js
+++ src/utils/dateFormater.js
@@ -1,7 +1,8 @@
 import { format } from 'date-fns'
 export const dateFormat = (dateString) => {
   if (!dateString) return null
-  const date = new Date(dateString)
+  const date = new Date(dateString.includes('Z') ? dateString : dateString + 'Z')
+  console.log(dateString, date)
   return format(date, 'Pp')
 }
 export const durationFormat = (durationString) => {

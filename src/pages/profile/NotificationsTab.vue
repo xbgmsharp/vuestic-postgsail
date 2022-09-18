121ddf9dc84f7a86fddcbad516c32533c628c8ff
xbgmsharp <xbgmsharp@gmail.com>
2022-09-18 15:28:21 +0000

    Add Profile page

---
+++ src/pages/profile/NotificationsTab.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pt-2">
+    <div class="row">
+      <div class="flex sm12 md6"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .va-input-wrapper {
+    margin-bottom: 1rem;
+  }
+</style>
--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,21 +1,47 @@
 <template>
   <div>
     <va-card>
-      <va-card-title>{{ $t('profile.title') }}</va-card-title>
+      <va-card-title>
+        {{ t('profile.title') }}
+      </va-card-title>
       <va-card-content>
-        <p>Profile content here</p>
+        <va-tabs v-model="activeTabName" grow>
+          <template #tabs>
+            <va-tab name="OverviewTab">
+              {{ t('profile.tabs.overview.title') }}
+            </va-tab>
+            <va-tab name="NotificationsTab">
+              {{ t('profile.tabs.notification.title') }}
+            </va-tab>
+          </template>
+        </va-tabs>
+        <va-separator />
+        <component :is="tabs[activeTabName]" @submit="submit" />
       </va-card-content>
     </va-card>
   </div>
 </template>
 
-<script>
-  import { defineComponent } from 'vue'
-  export default defineComponent({
-    data() {
-      return {}
-    },
-  })
+<script setup lang="ts">
+  import { defineAsyncComponent, ref } from 'vue'
+  import { useI18n } from 'vue-i18n'
+
+  const { t } = useI18n()
+
+  const tabs = {
+    OverviewTab: defineAsyncComponent(() => import('./OverviewTab.vue')),
+    NotificationsTab: defineAsyncComponent(() => import('./NotificationsTab.vue')),
+  }
+
+  const emit = defineEmits<{
+    (e: 'submit', data: any): void
+  }>()
+
+  const activeTabName = ref<keyof typeof tabs>('OverviewTab')
+
+  function submit(data: any) {
+    emit('submit', data)
+  }
 </script>
 
 <style lang="scss"></style>

99cbe9babf784aae8d06052b1a0cf139ec0f8028
xbgmsharp <xbgmsharp@gmail.com>
2023-01-09 10:59:31 +0000

    Add dependencies component for Grafana

---
+++ src/pages/grafana/Grafana.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Grafana Page</h1>
+  </div>
+</template>
+<script>
+  export default {
+    created() {
+      window.location.href = 'https://google.com/contact'
+    },
+  }
+</script>

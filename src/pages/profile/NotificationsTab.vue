046cc3084eb23edd258a84b63a58d23fab2c8d5e
xbgmsharp <xbgmsharp@gmail.com>
2023-02-28 09:01:23 +0000

    Add initial settings/profile page

--- src/pages/profile/NotificationsTab.vue
+++ src/pages/profile/NotificationsTab.vue
@@ -1,15 +1,159 @@
 <template>
-  <div class="pt-2">
-    <div class="row">
-      <div class="flex sm12 md6"></div>
+  <div class="notification-tab pt-2 layout">
+    <div class="va-table-responsive">
+      <table class="va-table full-table va-table--striped va-table--hoverable">
+        <tbody>
+          <tr>
+            <td>E-mail Notifications</td>
+            <td>
+              <div class="centerConainer">
+                <va-switch v-model="emailNotifications" size="small" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Phone Notifications</td>
+            <td>
+              <div class="centerConainer">
+                <QRCodeModal />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Alerting</td>
+            <td>
+              <div class="centerConainer">
+                <va-switch v-model="alerting" size="small" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Min Notification Interval(h)</td>
+            <td>
+              <div>
+                <va-input v-model="minNotificationInterval" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Wind Speed (kts)</td>
+            <td>
+              <div>
+                <va-input v-model="windSpeed" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Outdoor Temperature(C)</td>
+            <td>
+              <div>
+                <va-input v-model="outdoorTemp" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Indoor Temperature(C)</td>
+            <td>
+              <div>
+                <va-input v-model="indoorTemp" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Water Temperature</td>
+            <td>
+              <div>
+                <va-input v-model="waterTemp" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Water Depth(m)</td>
+            <td>
+              <div>
+                <va-input v-model="waterDepth" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Pressure(hpa)</td>
+            <td>
+              <div>
+                <va-input v-model="pressure" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Pressure Drop in 12 hours (hpa)</td>
+            <td>
+              <div>
+                <va-input v-model="presureDrop" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Battery Charge(%)</td>
+            <td>
+              <div>
+                <va-input v-model="batteryCharge" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Battery Voltage(V)</td>
+            <td>
+              <div>
+                <va-input v-model="batteryBoltage" class="mb-6" />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+  import { ref, onMounted } from 'vue'
+  import PostgSail from '../../services/postgsail.js'
+  import { useGlobalStore } from '../../stores/global-store'
+  import QRCodeModal from './QRCodeModal.vue'
+  import settingsData from '../../data/settings.json'
+  const { t } = useI18n()
+  const emailNotifications = ref(false)
+  const phoneNotifications = ref(false)
+  const alerting = ref(true)
+  const minNotificationInterval = ref(6)
+  const windSpeed = ref(0)
+  const outdoorTemp = ref(0)
+  const indoorTemp = ref(0)
+  const waterTemp = ref(0)
+  const waterDepth = ref(0)
+  const pressure = ref()
+  const presureDrop = ref()
+  const batteryCharge = ref()
+  const batteryBoltage = ref()
+</script>
 
 <style lang="scss" scoped>
+  .notification-tab {
+    &__item {
+      height: 55px;
+      &-icon {
+        min-width: 65px;
+        max-width: 65px;
+      }
+    }
+  }
   .va-input-wrapper {
     margin-bottom: 1rem;
+  }
+  .centerConainer {
+    display: flex;
+    justify-content: center;
+  }
+  .full-table {
+    width: 100%;
   }
 </style>
--- src/pages/profile/OverviewTab.vue
+++ src/pages/profile/OverviewTab.vue
@@ -1,89 +1,171 @@
 <template>
   <div class="overview-tab pt-2 layout">
-    <div class="mb-2"></div>
-    <div class="row">
-      <div class="flex xs12 xl6 mb-5">
-        <div class="overview-tab__item d-flex align--center">
-          <div class="overview-tab__item-icon fill-height mr-2">
-            <va-icon-vue />
-          </div>
-          <div class="text--bold">{{ t('dashboard.tabs.overview.built') }}</div>
-        </div>
-      </div>
-
-      <div class="flex xs12 xl6 mb-5">
-        <div class="overview-tab__item d-flex align--center">
-          <div class="overview-tab__item-icon fill-height mr-2">
-            <va-icon-responsive />
-          </div>
-          <div class="text--bold">{{ t('dashboard.tabs.overview.mobile') }}</div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="flex xs12 xl6 mb-5">
-        <div class="overview-tab__item d-flex align--center">
-          <div class="overview-tab__item-icon fill-height mr-2">
-            <va-icon-free />
-          </div>
-          <div class="text--bold">{{ t('dashboard.tabs.overview.free') }}</div>
-        </div>
-      </div>
-
-      <div class="flex xs12 xl6 mb-5">
-        <div class="overview-tab__item d-flex align--center">
-          <div class="overview-tab__item-icon fill-height mr-2">
-            <va-icon-rich />
-          </div>
-          <div class="text--bold">{{ t('dashboard.tabs.overview.components') }}</div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="flex xs12 xl6 mb-5">
-        <div class="overview-tab__item d-flex align--center">
-          <div class="overview-tab__item-icon fill-height mr-2">
-            <va-icon-fresh />
-          </div>
-          <div class="text--bold">{{ t('dashboard.tabs.overview.fresh') }}</div>
-        </div>
-      </div>
-
-      <div class="flex xs12 xl6">
-        <div class="overview-tab__item d-flex align--center">
-          <div class="overview-tab__item-icon fill-height mr-2">
-            <va-icon-clean-code />
-          </div>
-          <div class="text--bold">{{ t('dashboard.tabs.overview.nojQuery') }}</div>
-        </div>
-      </div>
+    <div class="va-table-responsive">
+      <table class="va-table full-table va-table--striped va-table--hoverable">
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>{{ fullName }}</td>
+          </tr>
+          <tr>
+            <td>Preferred Homepage</td>
+            <td>
+              <div class="centerContainer">
+                <va-select
+                  v-model="settings.preferences.preferred_homepage"
+                  :options="options"
+                  placeholder="Ship Logs"
+                  @change="
+                    UpdatePref(
+                      'preferred_homepage',
+                      settings.preferences.preferred_homepage ? settings.preferences.preferred_homepage : '',
+                    )
+                  "
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Use Imperial Units</td>
+            <td>
+              <div class="centerContainer">
+                <va-switch
+                  v-model="settings.preferences.use_imperial_units"
+                  size="small"
+                  @click="UpdatePref('use_imperial_units', settings.preferences.use_imperial_units)"
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Website</td>
+            <td>
+              <div class="centerContainer">
+                <va-input
+                  v-model="settings.preferences.website"
+                  class="mb-6"
+                  style=""
+                  @focusout="UpdatePref('website', settings.preferences.website)"
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Instagram Handle</td>
+            <td>
+              <div class="centerContainer">
+                <va-input
+                  v-model="settings.preferences.instagram_handle"
+                  class="mb-6"
+                  style=""
+                  @focusout="UpdatePref('instagram_handle', settings.preferences.instagram_handle)"
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Public Profile</td>
+            <td>
+              <div class="centerContainer">
+                <va-switch size="small" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Stats</td>
+            <td>
+              <div class="centerContainer">
+                <va-switch size="small" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Timelapse</td>
+            <td>
+              <div class="centerContainer">
+                <va-switch size="small" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Log List</td>
+            <td>
+              <div class="centerContainer">
+                <va-switch size="small" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Log Details</td>
+            <td>
+              <div class="centerContainer">
+                <va-switch size="small" />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import VaIconVue from '../../components/icons/VaIconVue.vue'
-  import VaIconFree from '../../components/icons/VaIconFree.vue'
-  import VaIconFresh from '../../components/icons/VaIconFresh.vue'
-  import VaIconResponsive from '../../components/icons/VaIconResponsive.vue'
-  import VaIconRich from '../../components/icons/VaIconRich.vue'
-  import VaIconCleanCode from '../../components/icons/VaIconCleanCode.vue'
-
   import { useI18n } from 'vue-i18n'
+  import { ref, onBeforeMount } from 'vue'
+  import { storeToRefs } from 'pinia'
+  import { useGlobalStore } from '../../stores/global-store'
+  import { useToast } from 'vuestic-ui'
+  import settingsData from '../../data/settings.json'
+
   const { t } = useI18n()
+  const { init: initToast } = useToast()
+  const GlobalStore = useGlobalStore()
+  const { settings } = storeToRefs(GlobalStore)
+  const { fetchSettings, updatePref } = GlobalStore
+
+  const options = ['Ship Logs', 'Stays', 'Units']
+  console.log(`${settings.value.first} ${settings.value.last}`)
+  const fullName = ref(`${settings.value.first} ${settings.value.last}`)
+
+  onBeforeMount(async () => {
+    console.log(`onBeforeMount Overviewtab`)
+    await fetchSettings()
+    console.log(`${settings.value.first} ${settings.value.last}`)
+  })
+
+  const UpdatePref = async (key: string, value: any) => {
+    if (!key) {
+      return
+    }
+    console.debug(`Updating ${key}:${value}`)
+    // Update GlobalStore should be automatic maybe need to use reactive()
+    // API Call api.update_user_preferences({ key: ${key}, value: ${value} }) from the store
+    const response = await updatePref(key, value)
+    // Notify user on success or failure using va-toast.
+    initToast({
+      message: response.ok ? `Successfully updated ${key} with ${value}` : `Error updated ${key} with ${value}`,
+      position: 'top-right',
+      color: 'primary',
+      //color: response.ok ? 'success' : 'warning',
+    })
+  }
 </script>
 
 <style lang="scss">
   .overview-tab {
     &__item {
       height: 55px;
-
       &-icon {
         min-width: 65px;
         max-width: 65px;
       }
     }
+  }
+  .centerContainer {
+    display: flex;
+    justify-content: center;
+  }
+  .full-table {
+    width: 100%;
   }
 </style>

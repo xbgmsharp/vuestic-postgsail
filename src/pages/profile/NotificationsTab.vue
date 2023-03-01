4bf514e515501d931e0c471636bb967651684f8a
xbgmsharp <xbgmsharp@gmail.com>
2023-03-01 11:41:08 +0000

    Update profile page with settings.preferences tab. Still issue merging default value from store

--- src/pages/profile/NotificationsTab.vue
+++ src/pages/profile/NotificationsTab.vue
@@ -4,109 +4,138 @@
       <table class="va-table full-table va-table--striped va-table--hoverable">
         <tbody>
           <tr>
-            <td>E-mail Notifications</td>
+            <td>{{ t('profile.email_notifications') }}</td>
             <td>
-              <div class="centerConainer">
-                <va-switch v-model="emailNotifications" size="small" />
+              <div class="centerContainer">
+                <va-switch v-model="settings.preferences.email_notifications" size="small" />
               </div>
             </td>
           </tr>
           <tr>
-            <td>Phone Notifications</td>
+            <td>{{ t('profile.phone_notifications') }}</td>
             <td>
-              <div class="centerConainer">
-                <QRCodeModal />
+              <div class="centerContainer">
+                <va-switch v-model="settings.preferences.phone_notifications" size="small" @click="handleLink()" />
               </div>
             </td>
           </tr>
+          <template v-if="settings.preferences.phone_notifications">
+            <tr>
+              <td>{{ t('profile.pushover') }}</td>
+              <td>
+                <div class="centerContainer">
+                  <a :href="pushover_link" target="_bank">Link Your Pushover Account to Enable</a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.telegram_bot') }}</td>
+              <td>
+                <div class="centerContainer">
+                  <a :href="telegram_link" target="_bank">Add PostgSail bot to a group or direct chat.</a>
+                </div>
+              </td>
+            </tr>
+          </template>
           <tr>
-            <td>Alerting</td>
+            <td>{{ t('profile.alerting') }}</td>
             <td>
-              <div class="centerConainer">
-                <va-switch v-model="alerting" size="small" />
+              <div class="centerContainer">
+                <va-switch v-model="settings.preferences.alerting" size="small" />
               </div>
             </td>
           </tr>
-          <tr>
-            <td>Min Notification Interval(h)</td>
-            <td>
-              <div>
-                <va-input v-model="minNotificationInterval" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Wind Speed (kts)</td>
-            <td>
-              <div>
-                <va-input v-model="windSpeed" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Outdoor Temperature(C)</td>
-            <td>
-              <div>
-                <va-input v-model="outdoorTemp" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Indoor Temperature(C)</td>
-            <td>
-              <div>
-                <va-input v-model="indoorTemp" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Water Temperature</td>
-            <td>
-              <div>
-                <va-input v-model="waterTemp" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Water Depth(m)</td>
-            <td>
-              <div>
-                <va-input v-model="waterDepth" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Pressure(hpa)</td>
-            <td>
-              <div>
-                <va-input v-model="pressure" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Pressure Drop in 12 hours (hpa)</td>
-            <td>
-              <div>
-                <va-input v-model="presureDrop" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Battery Charge(%)</td>
-            <td>
-              <div>
-                <va-input v-model="batteryCharge" class="mb-6" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Battery Voltage(V)</td>
-            <td>
-              <div>
-                <va-input v-model="batteryBoltage" class="mb-6" />
-              </div>
-            </td>
-          </tr>
+          <template v-if="settings.preferences.alerting">
+            <tr>
+              <td>{{ t('profile.min_notification_interval') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.min_notification_interval" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.high_wind_speed_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.high_wind_speed_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_outdoor_temperature_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.low_outdoor_temperature_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_indoor_temperature_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.low_indoor_temperature_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_water_temperature_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.low_water_temperature_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_water_depth_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.low_water_depth_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_pressure_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.low_pressure_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.high_pressure_drop_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.high_pressure_drop_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_battery_charge_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input v-model="settings.preferences.alerting.low_battery_charge_threshold" class="mb-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.low_battery_voltage_threshold') }}</td>
+              <td>
+                <div>
+                  <va-input
+                    v-model="settings.preferences.alerting.low_battery_voltage_threshold"
+                    class="mb-6"
+                    @focusout="
+                      UpdatePref(
+                        'low_battery_voltage_threshold',
+                        settings.preferences.alerting.low_battery_voltage_threshold,
+                      )
+                    "
+                  />
+                </div>
+              </td>
+            </tr>
+          </template>
         </tbody>
       </table>
     </div>
@@ -115,25 +144,63 @@
 
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n'
-  import { ref, onMounted } from 'vue'
-  import PostgSail from '../../services/postgsail.js'
+  import { ref, onBeforeMount } from 'vue'
+  import { storeToRefs } from 'pinia'
   import { useGlobalStore } from '../../stores/global-store'
-  import QRCodeModal from './QRCodeModal.vue'
+  import PostgSail from '../../services/postgsail.js'
+  import { useToast } from 'vuestic-ui'
   import settingsData from '../../data/settings.json'
+
   const { t } = useI18n()
-  const emailNotifications = ref(false)
-  const phoneNotifications = ref(false)
-  const alerting = ref(true)
-  const minNotificationInterval = ref(6)
-  const windSpeed = ref(0)
-  const outdoorTemp = ref(0)
-  const indoorTemp = ref(0)
-  const waterTemp = ref(0)
-  const waterDepth = ref(0)
-  const pressure = ref()
-  const presureDrop = ref()
-  const batteryCharge = ref()
-  const batteryBoltage = ref()
+  const { init: initToast } = useToast()
+  const GlobalStore = useGlobalStore()
+  const { settings } = storeToRefs(GlobalStore)
+  const { fetchSettings, updatePref } = GlobalStore
+
+  onBeforeMount(async () => {
+    console.log(`onBeforeMount Notificationtab`)
+    await fetchSettings()
+    console.log(`${settings.value.first} ${settings.value.last}`)
+  })
+  // TODO Issue getting default value as per store setup
+  const isBusy = ref(false)
+  const apiError = ref('')
+  const pushover_link = ref('')
+  const telegram_link = ref('https://t.me/pgsail_bot')
+
+  async function handleLink() {
+    isBusy.value = true
+    apiError.value = null
+    const api = new PostgSail()
+    try {
+      const response = await api.pushover_link()
+      if (response.data && response.data.pushover_link && response.data.pushover_link.link) {
+        pushover_link.value = response.data.pushover_link.link
+      }
+    } catch (e) {
+      apiError.value = e
+      console.error('Error generating pushover subscription link', apiError.value)
+    } finally {
+      isBusy.value = false
+    }
+  }
+
+  const UpdatePref = async (key: string, value: any) => {
+    if (!key) {
+      return
+    }
+    console.debug(`Updating ${key}:${value}`)
+    // Update GlobalStore should be automatic maybe need to use reactive()
+    // API Call api.update_user_preferences({ key: ${key}, value: ${value} }) from the store
+    const response = await updatePref(key, value)
+    // Notify user on success or failure using va-toast.
+    initToast({
+      message: response.ok ? `Successfully updated ${key} with ${value}` : `Error updated ${key} with ${value}`,
+      position: 'top-right',
+      color: 'primary',
+      //color: response.ok ? 'success' : 'warning',
+    })
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -149,7 +216,7 @@
   .va-input-wrapper {
     margin-bottom: 1rem;
   }
-  .centerConainer {
+  .centerContainer {
     display: flex;
     justify-content: center;
   }
--- src/pages/profile/OverviewTab.vue
+++ src/pages/profile/OverviewTab.vue
@@ -4,21 +4,23 @@
       <table class="va-table full-table va-table--striped va-table--hoverable">
         <tbody>
           <tr>
-            <td>Name</td>
+            <td>
+              <b>{{ t('profile.name') }}</b>
+            </td>
             <td>{{ fullName }}</td>
           </tr>
           <tr>
-            <td>Preferred Homepage</td>
+            <td>{{ t('profile.preferred_homepage') }}</td>
             <td>
               <div class="centerContainer">
                 <va-select
                   v-model="settings.preferences.preferred_homepage"
                   :options="options"
-                  placeholder="Ship Logs"
+                  placeholder="Dashboard"
                   @change="
                     UpdatePref(
                       'preferred_homepage',
-                      settings.preferences.preferred_homepage ? settings.preferences.preferred_homepage : '',
+                      settings.preferences.preferred_homepage ? settings.preferences.preferred_homepage : 0,
                     )
                   "
                 />
@@ -26,7 +28,7 @@
             </td>
           </tr>
           <tr>
-            <td>Use Imperial Units</td>
+            <td>{{ t('profile.use_imperial_units') }}</td>
             <td>
               <div class="centerContainer">
                 <va-switch
@@ -38,7 +40,7 @@
             </td>
           </tr>
           <tr>
-            <td>Website</td>
+            <td>{{ t('profile.website') }}</td>
             <td>
               <div class="centerContainer">
                 <va-input
@@ -51,58 +53,104 @@
             </td>
           </tr>
           <tr>
-            <td>Instagram Handle</td>
+            <td>{{ t('profile.instagram_handle') }}</td>
             <td>
               <div class="centerContainer">
                 <va-input
                   v-model="settings.preferences.instagram_handle"
                   class="mb-6"
-                  style=""
                   @focusout="UpdatePref('instagram_handle', settings.preferences.instagram_handle)"
                 />
               </div>
             </td>
           </tr>
           <tr>
-            <td>Public Profile</td>
+            <td>{{ t('profile.public_profile') }}</td>
             <td>
               <div class="centerContainer">
-                <va-switch size="small" />
+                <va-switch
+                  v-model="settings.preferences.public_profile"
+                  size="small"
+                  @click="
+                    UpdatePref(
+                      'public_profile',
+                      settings.preferences.public_profile ? settings.preferences.public_profile : false,
+                    )
+                  "
+                />
               </div>
             </td>
           </tr>
-          <tr>
-            <td>Stats</td>
-            <td>
-              <div class="centerContainer">
-                <va-switch size="small" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Timelapse</td>
-            <td>
-              <div class="centerContainer">
-                <va-switch size="small" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Log List</td>
-            <td>
-              <div class="centerContainer">
-                <va-switch size="small" />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>Log Details</td>
-            <td>
-              <div class="centerContainer">
-                <va-switch size="small" />
-              </div>
-            </td>
-          </tr>
+          <template v-if="settings.preferences.public_profile">
+            <tr>
+              <td>{{ t('profile.public_stats') }}</td>
+              <td>
+                <div class="centerContainer">
+                  <va-switch
+                    v-model="settings.preferences.public_stats"
+                    size="small"
+                    @click="
+                      UpdatePref(
+                        'public_stats',
+                        settings.preferences.public_stats ? settings.preferences.public_stats : true,
+                      )
+                    "
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.public_timelapse') }}</td>
+              <td>
+                <div class="centerContainer">
+                  <va-switch
+                    v-model="settings.preferences.public_timelapse"
+                    size="small"
+                    @click="
+                      UpdatePref(
+                        'public_timelapse',
+                        settings.preferences.public_timelapse ? settings.preferences.public_timelapse : true,
+                      )
+                    "
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.public_logs_list') }}</td>
+              <td>
+                <div class="centerContainer">
+                  <va-switch
+                    v-model="settings.preferences.public_logs_list"
+                    size="small"
+                    @click="
+                      UpdatePref(
+                        'public_logs_list',
+                        settings.preferences.public_logs_list ? settings.preferences.public_logs_list : true,
+                      )
+                    "
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ t('profile.public_logs') }}</td>
+              <td>
+                <div class="centerContainer">
+                  <va-switch
+                    v-model="settings.preferences.public_logs"
+                    size="small"
+                    @click="
+                      UpdatePref(
+                        'public_logs',
+                        settings.preferences.public_logs ? settings.preferences.public_logs : false,
+                      )
+                    "
+                  />
+                </div>
+              </td>
+            </tr>
+          </template>
         </tbody>
       </table>
     </div>
@@ -123,10 +171,10 @@
   const { settings } = storeToRefs(GlobalStore)
   const { fetchSettings, updatePref } = GlobalStore
 
-  const options = ['Ship Logs', 'Stays', 'Units']
+  const options = ['Dashboard', 'Ship Logs', 'Monitoring', 'Statistics']
   console.log(`${settings.value.first} ${settings.value.last}`)
   const fullName = ref(`${settings.value.first} ${settings.value.last}`)
-
+  // TODO Issue getting default value as per store setup
   onBeforeMount(async () => {
     console.log(`onBeforeMount Overviewtab`)
     await fetchSettings()
@@ -167,5 +215,8 @@
   }
   .full-table {
     width: 100%;
+  }
+  .form-control {
+    background-color: #fff;
   }
 </style>

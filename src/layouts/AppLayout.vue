63d2a7290fc4f89368151fe889bb418ffd995a11
xbgmsharp <xbgmsharp@gmail.com>
2023-03-01 11:50:13 +0000

    Update Applayout using GlobalStore

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,29 +1,42 @@
 <template>
-  <div class="app-layout">
-    <navbar />
-    <div class="app-layout__content">
-      <div class="app-layout__sidebar-wrapper" :class="{ minimized: isSidebarMinimized }">
-        <div v-if="isFullScreenSidebar" class="d-flex justify-end">
-          <va-button class="px-4 py-4" icon="md_close" preset="plain" color="dark" @click="onCloseSidebarButtonClick" />
+  <template v-if="isLoading">
+    <div class="row justify-center">
+      <LoadingScreen />
+    </div>
+  </template>
+  <template v-if="!isLoading">
+    <div class="app-layout">
+      <navbar />
+      <div class="app-layout__content">
+        <div class="app-layout__sidebar-wrapper" :class="{ minimized: isSidebarMinimized }">
+          <div v-if="isFullScreenSidebar" class="d-flex justify-end">
+            <va-button
+              class="px-4 py-4"
+              icon="md_close"
+              preset="plain"
+              color="dark"
+              @click="onCloseSidebarButtonClick"
+            />
+          </div>
+          <sidebar
+            :width="sidebarWidth"
+            :minimized="isSidebarMinimized"
+            :minimized-width="sidebarMinimizedWidth"
+            :animated="!isMobile"
+          />
         </div>
-        <sidebar
-          :width="sidebarWidth"
-          :minimized="isSidebarMinimized"
-          :minimized-width="sidebarMinimizedWidth"
-          :animated="!isMobile"
-        />
-      </div>
-      <div class="app-layout__page">
-        <div class="layout fluid va-gutter-5">
-          <router-view />
+        <div class="app-layout__page">
+          <div class="layout fluid va-gutter-5">
+            <router-view />
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script setup>
-  import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
+  import { computed, onBeforeUnmount, onBeforeMount, onMounted, ref } from 'vue'
   import { storeToRefs } from 'pinia'
   import { onBeforeRouteUpdate } from 'vue-router'
 
@@ -31,6 +44,7 @@
 
   import Navbar from '../components/navbar/Navbar.vue'
   import Sidebar from '../components/sidebar/Sidebar.vue'
+  import LoadingScreen from '../components/loadingScreen.vue'
 
   import PostgSail from '../services/postgsail.js'
 
@@ -45,6 +59,8 @@
   const isMobile = ref(false)
   const isTablet = ref(false)
   const { isSidebarMinimized } = storeToRefs(GlobalStore)
+  const { fetchSettings } = GlobalStore
+
   const checkIsTablet = () => window.innerWidth <= tabletBreakPointPX
   const checkIsMobile = () => window.innerWidth <= mobileBreakPointPX
 
@@ -57,14 +73,30 @@
     sidebarWidth.value = isTablet.value ? '100%' : '16rem'
   }
 
+  const isLoading = ref(true)
   const isBusy = ref(false)
   const apiError = ref(null)
 
   onMounted(() => {
+    isLoading.value = false
     window.addEventListener('resize', onResize)
+    //fetchSettings()
+    //console.log(GlobalStore.userName)
+    //GlobalStore.userName = userName.value
+    //changeUserName(userName.value)
+    /*
     if (localStorage.getItem('settings') === null) {
       handleSettings()
     }
+    */
+  })
+
+  onBeforeMount(async () => {
+    console.log('onBeforeMount AppLayout')
+    if (!Object.keys(GlobalStore.settings).length) {
+      await fetchSettings()
+    }
+    console.log(GlobalStore.settings)
   })
 
   const handleSettings = async () => {
@@ -74,8 +106,11 @@
     try {
       const response = await api.settings()
       if (response.data && response.data.settings) {
-        localStorage.setItem('settings', JSON.stringify(response.data.settings))
+        console.log(response.data.settings)
+        //localStorage.setItem('settings', JSON.stringify(response.data.settings))
         GlobalStore.userName = response.data.settings.username
+        GlobalStore.settings = response.data.settings
+        //changeUserName(response.data.settings.username)
       } else {
         throw {
           response: { data: { message: 'Wrong API response. Expected array, got ' + typeof response.data + '.' } },

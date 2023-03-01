4d88d51fc8573e4e268b4fb8de9480e1006c5081
xbgmsharp <xbgmsharp@gmail.com>
2023-03-01 17:36:13 +0000

    Add kelvin To Fahrenheit formater

--- src/utils/temperatureFormater.js
+++ src/utils/temperatureFormater.js
@@ -4,3 +4,10 @@ export const kelvinToCelsius = (deg) => {
   }
   return Math.round((deg - 273.15) * 10) / 10
 }
+
+export const kelvinToFahrenheit = (deg) => {
+  if (deg == null) {
+    return null
+  }
+  return Math.round((deg - 273.15) * 9) / 5 + 32
+}

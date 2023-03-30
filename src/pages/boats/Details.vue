c046779e17972fcc3942654e483e3a6e94e666c2
xbgmsharp <xbgmsharp@gmail.com>
2023-03-30 07:52:29 +0000

    Update boat details page, add icon for extra MMSI links

--- src/pages/boats/Details.vue
+++ src/pages/boats/Details.vue
@@ -26,16 +26,16 @@
                 <dt class="flex xs12 md6 pa-2 font-bold">VesselFinder</dt>
                 <dd class="flex xs12 md6 pa-2">
                   <a :href="`https://www.vesselfinder.com/vessels/details/${item.mmsi}`" target="_blank"
-                    >VesselFinder</a
-                  >
+                    >VesselFinder <va-icon name="fa-external-link" size="small"
+                  /></a>
                 </dd>
                 <dt class="flex xs12 md6 pa-2 font-bold">MarineVesselTraffic</dt>
                 <dd class="flex xs12 md6 pa-2">
                   <a
                     :href="`https://www.marinevesseltraffic.com/2013/06/mmsi-number-search.html?mmsi=${item.mmsi}`"
                     target="_blank"
-                    >MarineVesselTraffic</a
-                  >
+                    >MarineVesselTraffic <va-icon name="fa-external-link" size="small"
+                  /></a>
                 </dd>
               </template>
             </dl>

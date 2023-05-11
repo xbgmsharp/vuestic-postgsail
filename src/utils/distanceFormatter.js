7c9976fee2b4021cf8885bd9227fce184adc0bbe
xbgmsharp <xbgmsharp@gmail.com>
2023-05-11 10:35:42 +0000

    Update distance formatter, distance should always be in NM

--- src/utils/distanceFormatter.js
+++ src/utils/distanceFormatter.js
@@ -5,7 +5,8 @@ const { t } = i18n.global,
 
 export const distanceFormat = (val, trad = 'units.distance.miles') => {
   if (!val) return null
-  const n = GlobalStore.imperialUnits ? parseFloat(val) : ((trad = 'units.distance.kilometres'), milesToKm(val))
+  //const n = GlobalStore.imperialUnits ? parseFloat(val) : ((trad = 'units.distance.kilometres'), milesToKm(val))
+  const n = parseFloat(val)
   const count = { n: n % 1 ? parseFloat(n.toFixed(2)) : n }
   return t(trad, count)
 }

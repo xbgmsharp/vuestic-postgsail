00c910efca8c5c9560a45c7a558bc6be3c893a0e
xbgmsharp <xbgmsharp@gmail.com>
2023-06-27 15:57:09 +0000

    Update vue-routeir, add reset-password alias reset

--- src/router/index.ts
+++ src/router/index.ts
@@ -32,6 +32,7 @@ const routes: Array<RouteRecordRaw> = [
       {
         name: 'reset-password',
         path: 'reset-password',
+        alias: 'reset',
         component: () => import('../pages/auth/reset-password/ResetPassword.vue'),
       },
       {

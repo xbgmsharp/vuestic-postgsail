755af502df981f55d266b94b50d99cf3db5ca908
xbgmsharp <xbgmsharp@gmail.com>
2023-07-03 20:38:22 +0000

    Add conpoments no Data Yet!

---
+++ src/components/noDataScreen.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col" style="width: 100%">
+    <div id="" class="item">
+      <va-icon name="cloud_sync" :size="100" />
+      <p>
+        <!--         {{ t('monitoring.offline.offline') }} -->
+        Nothing here yet! Please configure your signalk-instance and start sailing!
+        <br />
+        If you just installed your collector and entered the Collector ID, have a look at your Monitoring page.
+        <br />
+        If you are not clear what you need to do for logging, please see "What do I need to use Saillogger?" question on
+        our FAQ.
+        <br />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Offline',
+  }
+</script>
+
+<script setup>
+  import { useI18n } from 'vue-i18n'
+  const { t } = useI18n()
+</script>
+
+<style>
+  .item {
+    /*border: 1px solid var(--va-background-border);*/
+    background-color: var(--va-background-primary);
+    text-align: center;
+  }
+</style>

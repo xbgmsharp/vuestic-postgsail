e6789dad42c7acf76f48b0234c1b89f05b81e276
xbgmsharp <xbgmsharp@gmail.com>
2023-10-25 10:01:39 +0000

    Update speed formater, in case of 1.x

--- src/utils/speedFormatter.js
+++ src/utils/speedFormatter.js
@@ -4,5 +4,9 @@ export const speedFormat = (val, trad = 'logs.log.speed_knots') => {
   if (!val) return null
   const n = parseFloat(val)
   const count = { n: n % 1 ? parseFloat(n.toFixed(2)) : n }
+  //console.log(count)
+  if (count.n < 2) {
+    count.n = 1
+  }
   return t(trad, count)
 }

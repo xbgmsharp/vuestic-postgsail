20224e47dde7bd8c24b187dd62d5b36a36c41fef
xbgmsharp <xbgmsharp@gmail.com>
2023-11-26 20:51:10 +0000

    Update temperatureFormatter, add convertion base in use pref Fahrenheit or Celsius

--- src/utils/temperatureFormatter.js
+++ src/utils/temperatureFormatter.js
@@ -1,3 +1,6 @@
+import useGlobalStore from '../stores/global-store'
+const GlobalStore = useGlobalStore()
+
 export const kelvinToCelsius = (deg) => {
   if (deg == null) {
     return null
@@ -10,4 +13,12 @@ export const kelvinToFahrenheit = (deg) => {
     return null
   }
   return Math.round((deg - 273.15) * 9) / 5 + 32
+}
+
+export const kelvinToHuman = (deg) => {
+  if (deg == null) {
+    return null
+  }
+  //console.log('imperialUnits', GlobalStore.imperialUnits)
+  return GlobalStore.imperialUnits ? kelvinToFahrenheit(deg) : kelvinToCelsius(deg)
 }

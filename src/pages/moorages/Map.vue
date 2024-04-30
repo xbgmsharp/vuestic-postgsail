c1b7ba94853122c6e183d883486d83ae5dfb0501
Luka Renko <46861689+lukarenko@users.noreply.github.com>
2024-04-30 07:42:30 +0200

    fix map-zoom

--- src/pages/moorages/Map.vue
+++ src/pages/moorages/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <Map style="width: 100%; height: 80vh" :zoom="8" :moorage-map-id="0" />
+  <Map style="width: 100%; height: 80vh" :map-zoom="8" :moorage-map-id="0" />
 </template>
 
 <script setup>

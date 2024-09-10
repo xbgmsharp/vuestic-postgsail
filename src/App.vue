d223747b9b0352b119e743f202daa816aca51321
xbgmsharp <xbgmsharp@gmail.com>
2024-09-10 14:45:06 +0000

    Udpate App main css after vesutic-ui update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <style lang="scss">
   @import 'scss/main.scss';
   #app {
-    font-family: 'Source Sans Pro', Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Inter', Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;

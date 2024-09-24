fd7c4dbc5256cd11359a244b9801356c3a231fcb
xbgmsharp <xbgmsharp@gmail.com>
2024-09-24 19:07:01 +0000

    Update dashboard page, add deck.gl map, update echarts graph

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -159,6 +159,18 @@
     </va-card>
 
     <va-card class="col-span-12">
+      <va-card-title>{{ t('dashboard.overview') }}</va-card-title>
+      <template v-if="LogsImage">
+        <va-card-content v-if="LogsImage">
+          <lMapgl />
+        </va-card-content>
+      </template>
+      <template v-else>
+        <va-card-content>{{ t('nodata.nodata') }}</va-card-content>
+      </template>
+    </va-card>
+
+    <va-card class="col-span-12">
       <va-card-title>{{ t('dashboard.versions') }}</va-card-title>
       <va-card-content>
         Frontend version: {{ versions.web_version }}, VueJS: {{ version }}, Vite: {{ vite_version }}<br />
@@ -180,6 +192,7 @@
   //const Charts = defineAsyncComponent(() => import('./Charts.vue'))
   const Echarts = defineAsyncComponent(() => import('./Echarts.vue'))
   const lMap = defineAsyncComponent(() => import('../../components/maps/leafletMap.vue'))
+  const lMapgl = defineAsyncComponent(() => import('../../components/maps/leafletMapgl.vue'))
   import PostgSail from '../../services/api-client'
   import { fromNow, localTime } from '../../utils/dateFormatter.js'
   import { Moon } from 'lunarphase-js'
--- src/pages/dashboard/Echarts.vue
+++ src/pages/dashboard/Echarts.vue
@@ -4,39 +4,43 @@
       <va-card class="chart-widget col-span-12">
         <va-card-title>{{ t('dashboard.charts.mixedChart') }}</va-card-title>
         <va-card-content>
-          <EchartsMix />
+          <EchartsMix v-if="mixedChartDataComputed" :series="mixedChartDataComputed" />
         </va-card-content>
       </va-card>
       <va-card class="chart-widget col-span-12">
-        <va-card-title>{{ t('dashboard.charts.mixedChart') }}</va-card-title>
+        <va-card-title>{{ t('dashboard.charts.HeatmapChart') }}</va-card-title>
         <va-card-content>
-          <EchartsPie />
+          <EchartsHeatmap v-if="HeatmapChartComputed" :series="HeatmapChartComputed" />
         </va-card-content>
       </va-card>
-      <va-card class="chart-widget col-span-12">
-        <va-card-title>{{ t('dashboard.charts.mixedChart') }}</va-card-title>
-        <va-card-content>
-          <EchartsLine />
-        </va-card-content>
-      </va-card>
-      <va-card class="chart-widget col-span-12">
+    </div>
+    <!-- From stats -->
+    <div class="dashboard flex flex-wrap col-span-12 p-2 gap-4">
+      <va-card class="flex card-width">
         <va-card-title>{{ t('dashboard.charts.mixedChart') }}</va-card-title>
         <va-card-content>
           <EchartsDonught />
         </va-card-content>
       </va-card>
-      <va-card class="chart-widget col-span-12">
+      <va-card class="flex card-width">
         <va-card-title>{{ t('dashboard.charts.mixedChart') }}</va-card-title>
         <va-card-content>
-          <EchartsHeatmap />
+          <EchartsDonught />
         </va-card-content>
       </va-card>
     </div>
+    <!-- For logbook details -->
+    <va-card class="chart-widget col-span-12">
+      <va-card-title>{{ t('dashboard.charts.mixedChart') }}</va-card-title>
+      <va-card-content>
+        <EchartsLine />
+      </va-card-content>
+    </va-card>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref, computed, onMounted, watch, defineAsyncComponent } from 'vue'
+  import { ref, computed, onMounted, watch, defineAsyncComponent, PropType } from 'vue'
   import { useI18n } from 'vue-i18n'
   import EchartsMix from '../../components/echarts/mix.vue'
   import EchartsPie from '../../components/echarts/pie.vue'
@@ -50,6 +54,75 @@
 
   const CacheStore = useCacheStore()
   const { getAPI, logs_by_month, logs_by_year_by_month, logs_by_month_by_weekday } = storeToRefs(CacheStore)
+  console.log('echarts logs_by_month', logs_by_year_by_month.value)
+
+  function random_rgb_dark() {
+    var o = Math.floor,
+      r = Math.random,
+      s = 256
+    return 'rgb(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ')'
+  }
+
+  const series = [
+    {
+      name: 'Bar Dataset',
+      type: 'bar',
+      smooth: true,
+      tooltip: {
+        valueFormatter: function (value) {
+          return value
+        },
+      },
+      data: [1, 2, 4, 4, 3, 4, 0, 5, 1, 3, 0, 0],
+    },
+  ]
+  const mixedChartDataComputed = computed(() => {
+    if (logs_by_month.value.length == 0) return {}
+    let mymixedChartData = [...series]
+    mymixedChartData[0].data = logs_by_month.value
+    mymixedChartData[0].name = 'Total'
+    Object.entries(logs_by_year_by_month.value).forEach((elm, ind) => {
+      const line = structuredClone({
+        type: 'line',
+        smooth: true,
+      })
+      line.name = elm[0]
+      line.data = elm[1] as number[]
+      line.borderColor = random_rgb_dark()
+      line.backgroundColor = line.borderColor
+      mymixedChartData[ind + 1] = line
+    })
+    return mymixedChartData
+  })
+  const HeatmapChartComputed = computed(() => {
+    let matrix_data = null
+    if (logs_by_month_by_weekday.value.length != 0) {
+      matrix_data = logs_by_month_by_weekday.value
+    }
+    return matrix_data
+  })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .dashboard {
+    .va-icon {
+      fill: var(--va-text-primary);
+    }
+    .va-card {
+      margin-bottom: 0 !important;
+      &__title {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+    .card-width {
+      width: 100%;
+      @media (min-width: 768px) {
+        width: calc((100% - 2rem) / 2);
+      }
+      @media (min-width: 1024px) {
+        width: calc((100% - 2rem) / 3);
+      }
+    }
+  }
+</style>

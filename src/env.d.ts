3490ec7afadb40e979436132ef98f75206fd1497
xbgmsharp <xbgmsharp@gmail.com>
2026-06-14 12:08:56 +0000

    Update main config, add VueQueryPlugin support and add listener for new version

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,7 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+import 'vite-plugin-pwa/vue'
 
 declare const __APP_VERSION__: string
 declare const __VITE_VERSION__: string
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import i18n from './i18n'
 import { createVuestic } from 'vuestic-ui'
 import vuesticGlobalConfig from './services/vuestic-ui/global-config'
 import App from './App.vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
+window.addEventListener('vite:preloadError', (event) => {
+  console.warn('Chunk load error, reloading...', event)
+  window.location.reload()
+})
 
 const app = createApp(App)
 
 app.use(stores)
+app.use(VueQueryPlugin) // VueQuery before router
 app.use(router)
 app.use(i18n)
 app.use(createVuestic({ config: vuesticGlobalConfig }))
